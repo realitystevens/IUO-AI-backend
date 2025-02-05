9d998a98f144c3093f161769b7e1c03177155d67
Reality Stevens <stevensreality@outlook.com>
2025-02-05 09:50:39 +0000

    refactor: modified application workflow

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import nltk
+"""
+    IUO AI Project main application
+"""
+
 import os
 import sys
-import PyPDF2
 import google.generativeai as genai
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer
 
 from dotenv import load_dotenv
 load_dotenv()
 
+import nltk
 nltk.download('punkt_tab')
 
 
@@ -20,43 +20,24 @@ class IUOAIproject:
             GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
             genai.configure(api_key=GOOGLE_API_KEY)
             self.model = genai.GenerativeModel("gemini-1.5-pro-latest")
-            self.pdfText = ""
+            self.fileText = ""
         except Exception as e:
             print(f"Initialization error: {e}")
             sys.exit(1)
 
-    def extractPDFText(self, pdfPath):
+    def extractText(self, filePath):
         try:
-            text = ""
-            with open(pdfPath, "rb") as file:
-                reader = PyPDF2.PdfReader(file)
-                for page in reader.pages:
-                    pageText = page.extract_text()
-                    if pageText:
-                        text += "\n\n" + pageText
+            with open(filePath, "r", encoding="utf-8") as file:
+                text = file.read()
             return text
         except Exception as e:
-            return f"Error extracting text from PDF: {e}"
+            return f"Error accessing TXT file: {e}"
 
-    def loadPDF(self, pdfPath):
-        """Validate if PDF file exists"""
-        if not os.path.exists(pdfPath):
-            print(f"Error: File {pdfPath} does not exist")
-
-        """Set up extracted text from PDF"""
-        self.pdfText = self.extractPDFText(pdfPath)
-        if not self.pdfText:
-            print("Failed to extract text from PDF file on loading the file")
-            return False
-
-        print(
-            f"Successfully loaded PDF and extracted text. Total word count: {len(self.pdfText.split())}")
-        return True
 
     def askQuestion(self, question):
-        """Validate if PDF is loaded"""
-        if not self.pdfText:
-            return "Please load a PDF first using 'load [path to PDF]'"
+        self.fileText = self.extractText("summarized_IUO_prospectus_2016-2020.txt")
+        if not self.fileText:
+            return "Failed to extract text from TXT file"
 
         try:
             prompt = f"""
@@ -65,7 +46,7 @@ class IUOAIproject:
             If the answer is not in the document, say "I cannot find the answer in the provided document."
 
             Document Text:
-            {self.pdfText}
+            {self.fileText}
 
             Question: {question}
 
@@ -88,14 +69,11 @@ def main():
             Follow the instructions to proceed.
 
             Instructions:
-                1. To proceed with the application, PDF must be loaded.
-                    To load PDF, use the command keyword 'load [path to PDF]'.
-                    For example: load IUO_prospectus_2016-2020.pdf
-
-                2. After loading the PDF, use the command keyword 'ask' followed your question.
+                1. To proceed with the application, 
+                    use the command keyword 'ask', followed your question.
                     For example: ask what is the name of the Vice Chancellor
 
-                3. To exit the application, use the command keyword 'exit'.
+                2. To exit the application, use the command keyword 'exit'.
         """
     )
 
@@ -108,21 +86,16 @@ def main():
                 print("Exiting application")
                 break
 
-            """Load PDF command"""
-            if user_input.lower().startswith("load "):
-                pdfPath = user_input[5:].strip()
-                if project.loadPDF(pdfPath):
-                    print("PDF loaded successfully")
+            if user_input.lower() == "ask" or user_input.lower() == "ask ":
+                print("Please, provide a question")
                 continue
 
             """Ask question commmand"""
             if user_input.lower().startswith("ask "):
                 question = user_input[4:].strip()
-                if not question:
-                    print("Please, provide a question")
-                    continue
 
                 """Get and print the answer"""
+                print("Processing question...")
                 answer = project.askQuestion(question)
                 print("\nAnswer: ", answer)
                 continue
@@ -134,6 +107,9 @@ def main():
             print("\nOperation cancelled. Type 'exit' to quit")
         except Exception as e:
             print(f"An error occured: {e}")
+
+
+
 
 
 if __name__ == "__main__":
